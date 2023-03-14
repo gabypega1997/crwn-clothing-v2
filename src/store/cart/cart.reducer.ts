8fb3c34cc5ffc1d7d101462a2f98252214fecabb
Pescar Gabriel <57466117+gabypega1997@users.noreply.github.com>
2023-03-14 20:20:09 +0100

    Typescript (#1)
    * Moving category code into array of objects
    
    * fixing brackets
    
    * moving directory and category-item into separate components
    
    * updating our directory page with directory component and category item component
    
    * updating fonts, removing package-lock file
    
    * added react-router-dom and implemented basic routing
    
    * adding navigation bar and linking
    
    * setting up firebase and adding authentication
    
    * added our sign in component as well as firebase utils and brought in firebase libraries
    
    * created sign up component, the form input component, button component
    
    * creating sign in form and updating route to authentication
    
    * updating userProvider with userAuth state change watcher
    
    * adding base sign in / sign out with context without observable
    
    * updating sign in button with constant
    
    * rely on authChange observer for signout setting
    
    * updating with createUserDocument
    
    * updating function name
    
    * removing unncessary spaces
    
    * creating products context and product card
    
    * adding cart context and shopping icon as well as cart dropdown
    
    * added in our addToCart feature
    
    * created checkout page along with checkout items and checkout cart functionality
    
    * updating shop-data and components to use live code
    
    * created nested routes for shop, fixed styling clash and improved solution
    
    * adding styled-components and styling navigation bar
    
    * restyling button component with additional pattern
    
    * converted entire application over to styled-components
    
    * remove unnecessary link
    
    * adding redirects for netlify
    
    * adding directory routing
    
    * resetting for lesson
    
    * adding redirect for netlify
    
    * updating user Context to use reducer
    
    * updating categories and cart to use reducer
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * reverting categories reducer
    
    * removing console.log
    
    * updating user context to use user reducer
    
    * updating categories context into categories reducer, introduced reselect library
    
    * migrate all components to use cart reducer
    
    * adding redux-persist
    
    * moving redux-logger into middleware folder, added redux-devtools integration
    
    * adding redux-thunk and loading spinner
    
    * adding saga middleware
    
    * separating categories into saga
    
    * updating categories thunk to saga
    
    * creating checkUserSession method
    
    * updating checkUserSession saga
    
    * updating sign out saga and sign in sagas
    
    * setting up netlify function and stripe payment processing with card element
    
    * re-ordering hoooks
    
    * increasing size on form
    
    * updating naming convention for payment form container
    
    * create a enviorment to transcript the js code
    
    * change a shop-data.ts
    
    * change actions for categories to ts
    
    * convert categories to ts
    
    * finished  categories
    
    * create types and selector for cart
    
    * finished the ts for cart
    
    * create cart with ts
    
    ---------
    
    Co-authored-by: Yihua Zhang <yihuazhang2@gmail.com>
    Co-authored-by: Yihua Zhang <yihuazhang@Yihuas-MacBook-Pro.local>

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,32 @@
+import { AnyAction } from "redux";
+
+import { CartState } from "./cart.types";
+
+import {
+    setIsCartOpen,
+    setCartItems,
+} from "./cart.action";
+
+export const CART_INITIAL_STATE: CartState = {
+    isCartOpen: false,
+    cartItems: [],
+};
+
+export const cartReducer = (
+    state = CART_INITIAL_STATE,
+    action : AnyAction
+): CartState => {
+    if (setCartItems.match(action)) {
+        return {
+            ...state,
+            cartItems: action.payload,
+        };
+    }
+    if (setIsCartOpen.match(action)) {
+        return {
+            ...state,
+            isCartOpen: action.payload,
+        };
+    }
+    return state;
+};
